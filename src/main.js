ecb1f520299a32873d44c7355132ec30c23685ef
Artur Mingazhov <arthurming7@gmail.com>
2021-08-18 14:53:05 +0300

    lint

--- src/main.js
+++ src/main.js
@@ -1,33 +1,28 @@
-import Vue from 'vue';
-import App from './App.vue';
-import '@/assets/styles/app.css';
+import Vue from "vue";
+import App from "./App.vue";
+import "@/assets/styles/app.css";
 import "@/assets/styles/index.css";
-import { router } from './router';
-import VueMeta from 'vue-meta';
-import ToasterPlugin from '@/plugins/ToasterPlugin/plugin.js';
-import TheTopProgressBarPlugin from '@/plugins/TopProgressBar/plugin.js';
+import { router } from "./router";
+import VueMeta from "vue-meta";
+import ToasterPlugin from "@/plugins/ToasterPlugin/plugin.js";
+import TheTopProgressBarPlugin from "@/plugins/TopProgressBar/plugin.js";
 import { authApi } from "@/api/authApi";
 import store from "@/store/index.js";
-
 Vue.config.productionTip = false;
-Vue.use(VueMeta)
-Vue.use(ToasterPlugin)
-Vue.use(TheTopProgressBarPlugin, { router })
 
-authApi.fetchUser().then((user) => {
+Vue.use(VueMeta);
+Vue.use(ToasterPlugin);
+Vue.use(TheTopProgressBarPlugin, { router });
 
-  store.commit('auth/SET_USER', user)
-}).catch(() => {
-
-}).finally(() => {
-
-  new Vue({
-    router,
-    render: (h) => h(App),
-  }).$mount('#app');
-})
-
-
-
-
-
+authApi
+  .fetchUser()
+  .then((user) => {
+    store.commit("auth/SET_USER", user);
+  })
+  .catch(() => {})
+  .finally(() => {
+    new Vue({
+      router,
+      render: (h) => h(App),
+    }).$mount("#app");
+  });
